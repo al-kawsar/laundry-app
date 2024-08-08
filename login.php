0c0a3478c11f9dcabfbced97dc0f71e88f6ccbf1
Raihan Alkawsar <tlod9906@gmail.com>
2024-08-08 09:13:20 +0800

    feat: add login page with authentication for laundry management system

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+include 'config.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+
+    $sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $_SESSION['username'] = $username;
+        header("Location: index.php");
+    } else {
+        echo "Invalid username or password";
+    }
+}
+?>
+
+<form method="post" action="">
+    Username: <input type="text" name="username" required><br>
+    Password: <input type="password" name="password" required><br>
+    <button type="submit">Login</button>
+</form>
