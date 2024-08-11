9334689ea944ee5d83ef217a52a7a4931698bf65
Raihan Alkawsar <tlod9906@gmail.com>
2024-08-11 17:29:04 +0800

    feat: change background button create member

--- members/index.php
+++ members/index.php
@@ -50,7 +50,7 @@ $title = "Member";
                 <div class="card pb-4">
                     <div class="my-3 flex  items-center justify-end px-4 sm:px-5">
                         <button
-                            class="btn border border-info/30 bg-info/10 font-medium text-info  hover:bg-info/20 focus:bg-info/20 active:bg-info/25">
+                            class="btn bg-slate-150 font-medium text-slate-800 hover:bg-slate-200 focus:bg-slate-200 active:bg-slate-200/80 dark:bg-navy-500 dark:text-navy-50 dark:hover:bg-navy-450 dark:focus:bg-navy-450 dark:active:bg-navy-450/90">
                             <a href="./create.php">Add Member</a>
                         </button>
                     </div>
