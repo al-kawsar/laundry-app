e24afa45ff49a4e42524a6f10c3f71dee1d0d515
Raihan Alkawsar <tlod9906@gmail.com>
2024-08-11 16:46:12 +0800

    feat: enhance member UI with datatable, header and create api member

---
+++ members/api.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+include '../config.php';
+
+$response = array('results' => '', 'message' => '');
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+    $sql = "SELECT * FROM member";
+    $result = $conn->query($sql);
+    $data = [];
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = [
+            "id" => $row['id'],
+            "nama" => $row['nama'],
+            "alamat" => $row['alamat'],
+            "jenis_kelamin" => $row['jenis_kelamin'],
+            "tlp" => $row['tlp'],
+        ];
+    }
+
+
+    $response['message'] = 'Get all members.';
+    $response['results'] = $data;
+
+
+} else {
+    $response['status'] = 'error';
+    $response['message'] = 'Invalid request method.';
+}
+
+// Return the response as JSON
+header('Content-Type: application/json');
+echo json_encode($response);
